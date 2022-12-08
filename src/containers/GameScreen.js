eb7cda53aa9d4a44a00c2e1e0f3652e9973f0340
Edward Janson <99486978+edwardjanson@users.noreply.github.com>
2022-12-08 13:25:25 +0000

    :hammer: updated loading logic for apis

--- src/containers/GameScreen.js
+++ src/containers/GameScreen.js
@@ -14,6 +14,7 @@ const GameScreen = () => {
 
     // game logic
     const [gameLoaded, changeGameLoaded] = useState(false);
+    const [errorLoading, changeErrorLoading] = useState(false);
     const [gameStarted, changeGameState] = useState(false);
     const [winner, setWinner] = useState(null)
     const [playerTurn, changePlayerTurn] = useState(false);
@@ -37,19 +38,34 @@ const GameScreen = () => {
 
     useEffect( () => { 
         if (!gameLoaded) {
-            if (!trivimonCollection) getTrivimonCollection();
-            else if (!playerTrivimon) getTrivimon(setPlayerTrivimon, changePlayerHPremaining);
-            else if (!computerTrivimon) getTrivimon(setComputerTrivimon, changeComputerHPremaining);
-            else if (!trivia) getTrivia();
-            else if (!triviaToAnswer) {
+            const fetchApis = async () => {
+                try {
+                const pokeResponse = await fetch("https://pokeapi.co/api/v2/generation/1/")
+                const trivimons = await pokeResponse.json();
+        
+                const playerTrivimon = getTrivimon(trivimons.pokemon_species, setPlayerTrivimon, changePlayerHPremaining);
+                const computerTrivimon = getTrivimon(trivimons.pokemon_species, setComputerTrivimon, changeComputerHPremaining);
+
+                const triviaResponse = await fetch("https://opentdb.com/api.php?amount=50&difficulty=medium&type=multiple")
+                const trivia = await triviaResponse.json();
+
+                setTrivia(trivia.results);
+
+                // load starting game logic
+                playerTrivimon.pace >= computerTrivimon.pace ? changePlayerTurn(true) : changePlayerTurn(false);
                 const randomIndex = Math.floor(Math.random() * trivia.length);
                 changeTriviaToAnswer(trivia[randomIndex]);
-            } else {
-                playerTrivimon.pace >= computerTrivimon.pace ? changePlayerTurn(true) : changePlayerTurn(false);
-                changeGameLoaded(true);
-            }
-        } else {
 
+                changeGameLoaded(true);
+
+                } catch {
+                    changeErrorLoading(true);
+                }
+            }
+
+            fetchApis();
+        } 
+        else {
             if (!triviaToAnswer) {
                 const randomIndex = Math.floor(Math.random() * trivia.length);
                 changeTriviaToAnswer(trivia[randomIndex]);
@@ -93,7 +109,7 @@ const GameScreen = () => {
                 }
             }
         }
-    }, [gameStarted, trivimonCollection, playerTrivimon, computerTrivimon, playerHPremaining, computerHPremaining, playerTurn, selectedMove, textFinished, triviaToAnswer, triviaAnswered, trivia]);
+    }, [gameLoaded, errorLoading]);
 
 
     const onStartChange = () => {
@@ -126,59 +142,47 @@ const GameScreen = () => {
         (playerHPremaining - damageTotal) < 0 ? changePlayerHPremaining(0) : changePlayerHPremaining(Math.round(playerHPremaining - damageTotal))
     }
 
-    const getTrivia = () => {
-        fetch("https://opentdb.com/api.php?amount=50&difficulty=medium&type=multiple")
-        .then(res => res.json())
-        .then(trivia => setTrivia(trivia.results));
-    }
 
-    const getTrivimonCollection = () => {
-        fetch("https://pokeapi.co/api/v2/generation/1/")
-        .then(res => res.json())
-        .then(trivimon => setTrivimonCollection(trivimon.pokemon_species));
-    }
-
-    const getTrivimon = (allocateTrivimon, setHPRemaining) => {
-        let trivimonStats;
-        const moves = [];
-
+    const getTrivimon = async (trivimons, allocateTrivimon, setHPRemaining) => {
         const randomIndex = Math.floor(Math.random() * 150);
-        fetch(`https://pokeapi.co/api/v2/pokemon/${trivimonCollection[Number(randomIndex)].name}`)
-        .then(trivimonData => trivimonData.json())
-        .then(trivimon => {
-            trivimonStats = {
-                name: generateSlug(2, { format: "title", categories: {adjective: ["personality", "time", "shapes", "taste"]}}),
-                np: trivimon.stats[0].base_stat,
-                iq: trivimon.stats[1].base_stat,
-                resilience: trivimon.stats[2].base_stat,
-                pace: trivimon.stats[5].base_stat,
-                moves: trivimon.moves.filter(move => 
-                    move.version_group_details[0].level_learned_at === 1 
-                    && move.version_group_details[0].version_group.name === "red-blue"
-                    ).map(move => {
-                        return {name: move.move.name, url: move.move.url}
-                    }),
-                frontImage: trivimon.sprites.front_default,
-                backImage: trivimon.sprites.back_default
-            }
+        const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${trivimons[Number(randomIndex)].name}`)
+        const trivimonData = await response.json()
 
-            trivimonStats.moves.forEach(move => {
-                fetch(move.url)
-                .then(movesData => movesData.json())
-                .then(moveDetails => {
-                    moves.push({
-                        name: generateSlug(2, { format: "title", categories: {adjective: ["sounds", "touch"]}}),
-                        precision: moveDetails.accuracy ? moveDetails.accuracy : 90,
-                        competence: moveDetails.power ? moveDetails.power : 1
-                    })
-                });
-            });
+        const trivimonStats = {
+            name: generateSlug(2, { format: "title", categories: {adjective: ["personality", "time", "shapes", "taste"]}}),
+            np: trivimonData.stats[0].base_stat,
+            iq: trivimonData.stats[1].base_stat,
+            resilience: trivimonData.stats[2].base_stat,
+            pace: trivimonData.stats[5].base_stat,
+            moves: trivimonData.moves.filter(move => 
+                move.version_group_details[0].level_learned_at === 1 
+                && move.version_group_details[0].version_group.name === "red-blue"
+                ).map(move => {
+                    return {name: move.move.name, url: move.move.url}
+                }),
+            frontImage: trivimonData.sprites.front_default,
+            backImage: trivimonData.sprites.back_default
+        }
 
-            trivimonStats.moves = moves;
-            console.log("trivimonStats", trivimonStats)
-            allocateTrivimon(trivimonStats);
-            setHPRemaining(trivimonStats.np)
+        const movePromises = trivimonStats.moves.map(move => {
+            return fetch(move.url).then(response => response.json())
         });
+
+        await Promise.all(movePromises)
+            .then((moves) => {
+                const moveDetails = moves.map((move) => {
+                return {
+                    name: generateSlug(2, { format: "title", categories: {adjective: ["sounds", "touch"]}}),
+                    precision: move.accuracy ? move.accuracy : 90,
+                    competence: move.power ? move.power : 1
+                }})
+
+                trivimonStats.moves = moveDetails;
+        });
+
+        console.log("trivimonStats", trivimonStats)
+        allocateTrivimon(trivimonStats);
+        setHPRemaining(trivimonStats.np)
     }
 
     const updateSelectedMove = (move) => {
@@ -194,58 +198,59 @@ const GameScreen = () => {
     }
 
     return (
-        <div className="GameScreen">
-            {!gameStarted ? 
-                !winner ?
-                <div className="Start">
-                <Start winner={winner} onStartChange={onStartChange}/>
-                </div>
-                :
-                <div className="Start">
-                    <Start winner={winner} onNewGame={onNewGame} computerTrivimonName={computerTrivimon.name} onStartChange={onStartChange}/>
-                </div>
-            :
-                !gameLoaded ?
-                    <div className="Start">
-                        <p>Loading...</p>
-                    </div>
-                :
-                    <>
-                        <div className="Trivinoms">
-                            <div className="Player">
-                                <TrivimonName name={playerTrivimon.name}/>
-                                <TrivimonNPBar np={playerTrivimon.np} npLeft={playerHPremaining}/>
-                                <TrivimonNPValues np={playerTrivimon.np} npLeft={playerHPremaining}/>
-                                <TrivimonImage image={playerTrivimon.backImage}/>
-                            </div>
+        <>test</>
+        // <div className="GameScreen">
+        //     {!gameStarted ? 
+        //         !winner ?
+        //         <div className="Start">
+        //         <Start winner={winner} onStartChange={onStartChange}/>
+        //         </div>
+        //         :
+        //         <div className="Start">
+        //             <Start winner={winner} onNewGame={onNewGame} computerTrivimonName={computerTrivimon.name} onStartChange={onStartChange}/>
+        //         </div>
+        //     :
+        //         !gameLoaded ?
+        //             <div className="Start">
+        //                 <p>Loading...</p>
+        //             </div>
+        //         :
+        //             <>
+        //                 <div className="Trivinoms">
+        //                     <div className="Player">
+        //                         <TrivimonName name={playerTrivimon.name}/>
+        //                         <TrivimonNPBar np={playerTrivimon.np} npLeft={playerHPremaining}/>
+        //                         <TrivimonNPValues np={playerTrivimon.np} npLeft={playerHPremaining}/>
+        //                         <TrivimonImage image={playerTrivimon.backImage}/>
+        //                     </div>
 
-                            <div className="Computer">
-                                <TrivimonImage image={computerTrivimon.frontImage}/>
-                                <TrivimonName name={computerTrivimon.name}/>
-                                <TrivimonNPBar np={computerTrivimon.np} npLeft={computerHPremaining}/>
-                                <TrivimonNPValues np={computerTrivimon.np} npLeft={computerHPremaining}/>
-                            </div>
-                        </div>
+        //                     <div className="Computer">
+        //                         <TrivimonImage image={computerTrivimon.frontImage}/>
+        //                         <TrivimonName name={computerTrivimon.name}/>
+        //                         <TrivimonNPBar np={computerTrivimon.np} npLeft={computerHPremaining}/>
+        //                         <TrivimonNPValues np={computerTrivimon.np} npLeft={computerHPremaining}/>
+        //                     </div>
+        //                 </div>
                         
-                        <div className="InfoBoard">
-                            <InfoBoard 
-                                playerTrivimonName={playerTrivimon.name} 
-                                computerTrivimonName={computerTrivimon.name} 
-                                playerMoves={playerTrivimon.moves} 
-                                selectedMove={selectedMove} 
-                                onMoveSelection={updateSelectedMove}
-                                updateTextFinished={updateTextFinished}
-                                onMoveHover={onMoveHover}
-                                moveHovered={moveHovered}
-                                playerTurn={playerTurn}
-                                triviaToAnswer={triviaToAnswer}
-                                triviaAnswered={triviaAnswered}
-                                changeTriviaAnswered={changeTriviaAnswered}
-                                />
-                        </div>
-                    </>
-            }
-        </div>
+        //                 <div className="InfoBoard">
+        //                     <InfoBoard 
+        //                         playerTrivimonName={playerTrivimon.name} 
+        //                         computerTrivimonName={computerTrivimon.name} 
+        //                         playerMoves={playerTrivimon.moves} 
+        //                         selectedMove={selectedMove} 
+        //                         onMoveSelection={updateSelectedMove}
+        //                         updateTextFinished={updateTextFinished}
+        //                         onMoveHover={onMoveHover}
+        //                         moveHovered={moveHovered}
+        //                         playerTurn={playerTurn}
+        //                         triviaToAnswer={triviaToAnswer}
+        //                         triviaAnswered={triviaAnswered}
+        //                         changeTriviaAnswered={changeTriviaAnswered}
+        //                         />
+        //                 </div>
+        //             </>
+        //     }
+        // </div>
     );
 }
 
