52a3fc4f91745b83ee2706fc49887eae0253baa5
Edward Janson <99486978+edwardjanson@users.noreply.github.com>
2022-12-15 11:11:05 +0000

    :hammer: updated gameScreen to be mobile-friendly

--- src/components/TrivimonImage.js
+++ src/components/TrivimonImage.js
@@ -3,9 +3,7 @@ import { Pixelify } from "react-pixelify";
 
 const TrivimonImage = ({image}) => {
 
-    return <Pixelify width={image.includes("back") ? 250 : 175} 
-                    height={image.includes("back") ? 250 : 175} 
-                    pixelSize={18} src={image} c
+    return <Pixelify pixelSize={18} src={image} c
                     className={image.includes("back") ? "Player" : "Computer"}
             />
 }
